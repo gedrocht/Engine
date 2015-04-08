df2bd084468fc5e165ae4762cd37d0009d6fe5d3
gedrocht <cookiegedrocht@gmail.com>
2015-04-08 11:39:15 -0400

    Added more WildBunny AS→C++ code
    -Added eKeyCodes.h
    -Added eTileTypes.h

---
+++ GameEngine/eKeyCodes.h
@@ -0,0 +1,22 @@
+int kZero = 0x30;
+int kOne = 0x31;
+int kTwo = 0x32;
+int kThree = 0x33;
+int kFour = 0x34;
+int kFive = 0x35;
+int kSix = 0x36;
+int kSeven = 0x37;
+int kEight = 0x38;
+int kNine = 0x39;
+
+int kLeftArrow = 37;
+int kUpArrow = 38;
+int kRightArrow = 39;
+int kDownArrow = 40;
+
+int kEnter = 13;
+int kSpace = 32;
+
+int kS = 0x53;
+int kU = 0x55;
+int kP = 0x50;
---
+++ GameEngine/eTileTypes.h
@@ -0,0 +1,5 @@
+int kEmpty = 0x00;
+int kPlatform = 0x01;
+int kPlayer = 0x02;
+	
+int kInvalid = 0xff;
