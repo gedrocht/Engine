055f8e43ea3ddc073310705583e9daf32d77a89c
gedrocht <cookiegedrocht@gmail.com>
2015-04-10 00:35:22 -0400

    Continued WB integration

--- GameEngine/constants.h
+++ GameEngine/constants.h
@@ -4,6 +4,9 @@
 
 #define PI 3.141593
 
+#ifndef MIN_MAX
+#define MIN_MAX
+
 static float kGravity = 100;
 static float kTwoPi = PI*2;
 static float kMaxSpeed = 300;
@@ -13,16 +16,18 @@ static float kDesiredFps = 30.0;
 static float kPlaneHeight = 12;
 static int kPlayerWidth = 30;
 
-static int max( float a, float b ){
+static float _max( float a, float b ){
 	if( a > b )
 		return a;
 	else
 		return b;
 }
 
-static int min( float a, float b ){
+static float _min( float a, float b ){
 	if( a < b )
 		return a;
 	else
 		return b;
 }
+
+#endif
